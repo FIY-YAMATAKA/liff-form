9fdcff733d17534e10a6e554cdfdb2b1142b30a6
Takashi YAMAMOTO <105471172+FIY-YAMATAKA@users.noreply.github.com>
2024-04-01 11:57:34 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -28,9 +28,9 @@ function sendText(text) {
         'type': 'text',
         'text': text
     },{
-        "type": "sticker",
-        "packageId": "11539",
-        "stickerId": "52114113"
+         'type': 'sticker',
+        'packageId': '11539",
+        'stickerId': '52114113'
     }.{
         'type': 'text',
         'text': '送信が完了しました！'
